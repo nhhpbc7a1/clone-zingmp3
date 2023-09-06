b119daaa4d67bbd3fd8bcb754806f97563357b6b
Nguyễn Hoàng Huy <skill0sp1@gmail.com>
2023-09-07 00:03:31 +0700

    multi singers

--- main.js
+++ main.js
@@ -10,14 +10,37 @@ fetch(dataApi)
     .then(function(data) {
         songs = data.songs;
         user = data.users[0];
-        console.log(songs, user);
+        // console.log(songs, user);
+        // console.log(user.playing.songId);
+        var playAudio = document.getElementById('play-audio');
+        var img = playAudio.querySelector('img');
+        var infoBlock = playAudio.querySelector('.song__info');
+        var heart = playAudio.querySelector('.song__to-library')
+        
+        var id = user.playing.songId;
+        var thisSong = songs.find(function(song) {
+            return song.id === id;
+        });
+        img.src = `./assets/img/songs/song${id}.png`;
+
+        infoBlock.innerHTML = `
+           <p class="song__name">${thisSong.name}</p>
+        `;
+        thisSong.singers.forEach(function(singer) {
+            infoBlock.innerHTML += `
+                <a class="song__singer">${singer}</a>
+            `;
+        });
+        var isLiked = user.like.songs.some(function(song) {
+            return song === id;
+        });
+    
+        if (isLiked) {
+            heart.classList.remove('fa-regular');
+            heart.classList.add('fa-solid');
+            heart.classList.add('color-purple');
+        }
+    
     });
 
 
-{
-    var playAudio = document.getElementById('play-audio');
-    var img = playAudio.querySelector('img');
-    var name = playAudio.querySelector('name');
-    var singer = playAudio.querySelector('singer');
-
-}
